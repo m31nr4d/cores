9fd680fb4c546573e51bf581c7d30478e4befc44
David Paul-Beier <david.paul-beier@outlook.de>
2022-05-29 00:50:51 +0200

    MicroNMEA (teensydbg not tested)

--- user/main.cpp
+++ user/main.cpp
@@ -30,31 +30,106 @@
 
 #include <Arduino.h>
 
+#include <MicroNMEA.h>
+#include <mcurses.h>
+
+// Check we are using GDB Debugging and include the additional code
+#ifdef DEBUG
+#include <TeensyDebug.h>
+#endif
+
+// Refer to serial devices by use
+usb_serial_class& console = SerialUSB;
+HardwareSerial& gps = Serial1;
+
+char nmeaBuffer[1024];
+MicroNMEA nmea(nmeaBuffer, sizeof(nmeaBuffer));
+bool ledState = LOW;
+
 extern "C" int main(void)
 {
-#ifdef USING_MAKEFILE
 
-	// To use Teensy 4.0 without Arduino, simply put your code here.
-	// For example:
+    #ifdef DEBUG
+    while (SerialUSB1) {}    // Wait for Debugger connect
+    debug.begin(SerialUSB1);  // Start Debug Serial e.g. COM11
+    #endif
 
-	pinMode(13, OUTPUT);
+    console.begin(115200); // console
+    gps.begin(9600); // gps
 
+    pinMode(13, OUTPUT);
+    digitalWriteFast(13, LOW);
+    pinMode(25, OUTPUT);
+    digitalWriteFast(25, HIGH);
+    pinMode(26, OUTPUT);
+    digitalWriteFast(26, HIGH); // Active Low
 
 	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
+
+        digitalWriteFast(25, LOW);
+        while (gps.available()) {
+            char c = gps.read();
+            //console.print(c);
+            nmea.process(c);
+        }
+        digitalWriteFast(25, HIGH);
+        
+        // Output GPS information from previous second
+        console.print("Valid fix: ");
+        console.println(nmea.isValid() ? "yes" : "no");
+
+        console.print("Nav. system: ");
+        if (nmea.getNavSystem())
+          console.println(nmea.getNavSystem());
+        else
+          console.println("none");
+
+        console.print("Num. satellites: ");
+        console.println(nmea.getNumSatellites());
+
+        console.print("HDOP: ");
+        console.println(nmea.getHDOP() / 10., 1);
+
+        console.print("Date/time: ");
+        console.print(nmea.getYear());
+        console.print('-');
+        console.print(int(nmea.getMonth()));
+        console.print('-');
+        console.print(int(nmea.getDay()));
+        console.print('T');
+        console.print(int(nmea.getHour()));
+        console.print(':');
+        console.print(int(nmea.getMinute()));
+        console.print(':');
+        console.println(int(nmea.getSecond()));
+
+        long latitude_mdeg = nmea.getLatitude();
+        long longitude_mdeg = nmea.getLongitude();
+        console.print("Latitude (deg): ");
+        console.println(latitude_mdeg / 1000000., 6);
+
+        console.print("Longitude (deg): ");
+        console.println(longitude_mdeg / 1000000., 6);
+
+        long alt;
+        console.print("Altitude (m): ");
+        if (nmea.getAltitude(alt))
+          console.println(alt / 1000., 3);
+        else
+          console.println("not available");
+
+        console.print("Speed: ");
+        console.println(nmea.getSpeed() / 1000., 3);
+        console.print("Course: ");
+        console.println(nmea.getCourse() / 1000., 3);
+
+        console.println("-----------------------");
+        nmea.clear();
+
+        nmea.isValid() ? digitalWriteFast(26, LOW) : digitalWriteFast(26, HIGH);
+
 	}
 
 
-#else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
-#endif
 }
 
